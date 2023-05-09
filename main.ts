ad8de1cbc119ffaeb447244ce20a013e86689ff7
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-05-09 18:56:03 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,34 +1,52 @@
 function escreva () {
     basic.clearScreen()
-    led.plot(ponto, 2)
-    if (ponto == 0) {
+    led.plot(led2, 2)
+    if (led2 == 0) {
         basic.showString("A")
-        ponto = 2
-    } else if (ponto == 4) {
+        led2 = 2
+        A += 1
+        basic.showNumber(A)
+        basic.clearScreen()
+    } else if (led2 == 4) {
         basic.showString("B")
-        ponto = 2
+        led2 = 2
+        B += 1
+        basic.showNumber(B)
+        basic.clearScreen()
     }
 }
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("A")
+    basic.showNumber(B)
+    basic.showString("B")
+    basic.showNumber(A)
+    basic.clearScreen()
+    led.plot(led2, 2)
+})
 input.onButtonPressed(Button.A, function () {
     music.playTone(440, music.beat(BeatFraction.Whole))
-    ponto += -1
+    led2 += -1
     basic.clearScreen()
-    led.plot(ponto, 2)
+    led.plot(led2, 2)
     escreva()
 })
 input.onButtonPressed(Button.AB, function () {
-    ponto = 2
+    led2 = 2
     basic.clearScreen()
-    led.plot(ponto, 2)
+    led.plot(led2, 2)
 })
 input.onButtonPressed(Button.B, function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
-    ponto += 1
+    led2 += 1
     basic.clearScreen()
-    led.plot(ponto, 2)
+    led.plot(led2, 2)
     escreva()
 })
-let ponto = 0
-ponto = 2
+let B = 0
+let A = 0
+let led2 = 0
+led2 = 2
 basic.clearScreen()
-led.plot(ponto, 2)
+led.plot(led2, 2)
+A = 0
+B = 0
